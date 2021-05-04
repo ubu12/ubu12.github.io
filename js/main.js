26d1178f6f6728f239eb692603494a7dfc62bc1b
runes superior <65648335+ubu12@users.noreply.github.com>
2021-05-04 14:56:10 +1200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from '/js/threejs/three.module.js';
+import * as THREE from './js/threejs/three.module.js';
 import { PointerLockControls } from '/js/threejs/utils/PointerLockControls.js'
 let fpCamera, scene, renderer, geometry, material, mesh, controls;
 
@@ -7,7 +7,7 @@ init();
 function init() {
 
 	fpCamera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	fpCamera.position.z = 1;
+	camera.position.z = 1;
 	controls = new PointerLockControls(fpCamera, document.body)
 	scene = new THREE.Scene();
 
@@ -21,12 +21,45 @@ function init() {
 	renderer.setSize( window.innerWidth, window.innerHeight );
 	renderer.setAnimationLoop( animation );
 	document.body.appendChild( renderer.domElement );
-	document.body.addEventListener( 'click', function () {
 
-			controls.lock();
+	window.addEventListener("keydown", function(event){
+		switch (event){
+			case (event.code = KeyW):
+					console.log("w")
+				break;
+				case (event.code = KeyD):
+					console.log("d")
+				break;
+				case (event.code = KeyA):
+					console.log("A")
+					break;
+				case (event.code = KeyS):
+				console.log("S")
 
-		} );
+	
 
+
+		}
+	});
+	window.addEventListener("keyup", function(event){
+		switch (event){
+			case (event.code = KeyW):
+					console.log("wup")
+				break;
+				case (event.code = KeyD):
+					console.log("dup")
+				break;
+				case (event.code = KeyA):
+					console.log("aup")
+					break;
+				case (event.code = KeyS):
+				console.log("sup")
+
+	
+
+
+		}
+	});
 
 }
 
@@ -35,6 +68,6 @@ function animation( time ) {
 	mesh.rotation.x = time / 2000;
 	mesh.rotation.y = time / 1000;
 
-	renderer.render( scene, fpCamera );
+	renderer.render( scene, camera );
 
 }
