8758b36692d55d58fd5b98ddba7ef3b42b91a41b
runes superior <65648335+ubu12@users.noreply.github.com>
2021-05-04 14:34:23 +1200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from './js/threejs/three.module.js';
+import * as THREE from '/js/threejs/three.module.js';
 import { PointerLockControls } from '/js/threejs/utils/PointerLockControls.js'
 let camera, scene, renderer, geometry, material, mesh, controls;
 
