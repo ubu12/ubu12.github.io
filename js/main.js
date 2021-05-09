be99597dbb2e2b4eb9d82d9d50973ad839499722
runes superior <65648335+ubu12@users.noreply.github.com>
2021-05-10 10:20:06 +1200

    Update movement
    Add add code for movement towards camera via camera.position.x

--- js/main.js
+++ js/main.js
@@ -10,12 +10,11 @@ let moveLeft = false;
 let moveRight = false;
 let canJump = false;
 let velocity = 1;
-//main loop function
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////// scene/rendering code ////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 function init () {
- 
-
-
-     //give values to our variables
+     //give values to our variables and initliase our renderers
      //create a scene
      scene = new THREE.Scene();
      //create a new renderer
@@ -43,28 +42,67 @@ function init () {
          console.error( error );
 
         } );
-    //define our animation function
-    const animate = function () {
-        requestAnimationFrame( animate );
-        renderer.render( scene, fpCamera );
-    };
+            //define our animation function
+
+        const animate = function () {
+            requestAnimationFrame( animate );
+            renderer.render( scene, fpCamera );
+        };
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////// movement/interaction code ///////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    var vector = fpCamera.getWorldDirection();
+    angle = THREE.Math.radToDeg( Math.atan2(vector.x,vector.z) );  
+
     //hook camera with control module
     const controls = new PointerLockControls( fpCamera, document.body );
     // add event listener to show/hide a UI (e.g. the game's menu)
     document.addEventListener( 'click', function () {
-        //warning : in currect chrome build ther pointer lock api retrurns errors on call. https://bugs.chromium.org/p/chromium/issues/detail?id=1127920
+    // warning : in currect chrome build ther pointer lock api retrurns errors on call. https://bugs.chromium.org/p/chromium/issues/detail?id=1127920
         controls.lock();
     
     }, false);
+    if (moveForward == true) {     
+        fpCamera.position.x += velocity * Math.sin(angle);
+        fpCamera.position.z -= velocity * Math.cos(angle);
+    
+
+    }
+    // listen for keypresses
+    document.addEventListener("keydown", function (KeyboardEvent){
+        if (KeyboardEvent.key == "w") {
+            moveForward = true;
+        }
+        else if (KeyboardEvent.key == "a") {
+            moveLeft = true;
+        }
+        else if (KeyboardEvent.key == "s") {
+            moveBackward = true;
+        }
+        else if (KeyboardEvent.key == "d") {
+            moveRight = true;
+        }
+    })
+    document.addEventListener("keyup", function (KeyboardEvent){
+        if (KeyboardEvent.key == "w") {
+            moveForward = false;
+        }
+        else if (KeyboardEvent.key == "a") {
+            moveLeft = false;
+        }
+        else if (KeyboardEvent.key == "s") {    
+            moveBackward = false;
+        }
+        else if (KeyboardEvent.key == "d") {
+            moveRight = false;
+        }
+    })
     //animate 3d objects
     animate();
     // find camera angle relative to 0
- /*
-    var vector = fpCamera.getWorldDirection();
-    angle = THREE.Math.radToDeg( Math.atan2(vector.x,vector.z) );  
-    this.position.x += velocity * Math.sin(angle);
-    this.position.z -= velocity * Math.cos(angle);
-*/
+ 
+    
+
     
 
 }
